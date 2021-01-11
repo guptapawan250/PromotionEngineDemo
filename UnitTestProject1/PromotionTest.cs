587ad13c55fb6acd4511b71231fae29081ebb171
guptapawan250 <70570758+guptapawan250@users.noreply.github.com>
2021-01-11 12:23:04 +0530

    Test project commit

---
+++ UnitTestProject1/PromotionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProblemStatementForPromotionEngine;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PromotionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            IProductDetails prodDetail = new ProductDetails();
+            ProductManager prodMgr = new ProductManager(prodDetail);
+            List<Product> products = prodMgr.GetProductList();
+            
+            Assert.IsNotNull(products);
+
+        }
+
+        [TestMethod]
+        public void GetTotalAmountTest()
+        {
+            IProductDetails prodDetail = new ProductDetails();
+            ProductManager prodMgr = new ProductManager(prodDetail);
+            List<Product> prodLst = new List<Product>()
+            {
+                new Product("A", 5),
+                new Product("B", 5),
+                new Product("C", 1),
+                new Product("D", 0)
+            };
+           var result = prodMgr.GetTotalPrice(prodLst);
+           Assert.IsNotNull(result);
+        }
+    }
+}
